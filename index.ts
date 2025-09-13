e34388d062c4e02ac479a3685de70a8e1bce8164
BishanathTarafder <bishanathtarafder@gmail.com>
2025-09-13 09:51:38 +0600

    run both apps together

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!");
+import concurrent from 'concurrently'
+concurrent([
+    {
+        name: 'server',
+        command: 'bun run dev',
+        cwd: 'packages/server',
+        prefixColor: 'blue',
+    },
+    {
+        name: 'client',
+        command: 'bun run dev',
+        cwd: 'packages/client',
+        prefixColor: 'green',
+    }
+])
