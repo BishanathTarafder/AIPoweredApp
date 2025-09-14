888810efcc57321fc3f269afd1bcc5661f40f028
BishanathTarafder <bishanathtarafder@gmail.com>
2025-09-14 11:40:07 +0600

    set up prettier

--- index.ts
+++ index.ts
@@ -1,15 +1,15 @@
-import concurrent from 'concurrently'
+import concurrent from 'concurrently';
 concurrent([
-    {
-        name: 'server',
-        command: 'bun run dev',
-        cwd: 'packages/server',
-        prefixColor: 'blue',
-    },
-    {
-        name: 'client',
-        command: 'bun run dev',
-        cwd: 'packages/client',
-        prefixColor: 'green',
-    }
-])
+   {
+      name: 'server',
+      command: 'bun run dev',
+      cwd: 'packages/server',
+      prefixColor: 'blue',
+   },
+   {
+      name: 'client',
+      command: 'bun run dev',
+      cwd: 'packages/client',
+      prefixColor: 'green',
+   },
+]);
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -8,12 +8,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!!');
-})
+   res.send('Hello World!!');
+});
 app.get('/api/hello', (req: Request, res: Response) => {
-    res.json('Hello World!!!');
-})
+   res.json('Hello World!!!');
+});
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+   console.log(`Server is running at http://localhost:${port}`);
 });
