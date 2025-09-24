c1189110ff521a2a0ca59e49d25954521215b957
BishanathTarafder <bishanathtarafder@gmail.com>
2025-09-24 22:15:23 +0600

    Style applied to the Displaying Reviews

---
+++ packages/client/src/components/reviews/ReviewList.tsx
@@ -0,0 +1,73 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Skeleton from 'react-loading-skeleton';
+import StarRating from './StarRating';
+import type { PiPlaceholder } from 'react-icons/pi';
+
+type Props = {
+   productId: number;
+};
+
+type Review = {
+   id: number;
+   author: string;
+   content: string;
+   rating: number;
+   createdAt: string;
+};
+
+type GetReviewsResponse = {
+   summary: string | null;
+   reviews: Review[];
+};
+
+const ReviewList = ({ productId }: Props) => {
+   const [reviewData, setReviewData] = useState<GetReviewsResponse>();
+   const [isLoading, setIsLoading] = useState(false);
+
+   const fetchReviews = async () => {
+      setIsLoading(true);
+      const { data } = await axios.get<GetReviewsResponse>(
+         `/api/products/${productId}/reviews`
+      );
+      setReviewData(data);
+      setIsLoading(false);
+   };
+
+   useEffect(() => {
+      fetchReviews();
+   }, []);
+
+   if (isLoading) {
+      return (
+         <div className="flex flex-col gap-5">
+            {[1, 2, 3].map((i) => (
+               <div key={i}>
+                  <Skeleton width={150} />
+                  <Skeleton width={100} />
+                  <Skeleton count={7} />
+                  <Skeleton width={80} />
+               </div>
+            ))}
+         </div>
+      );
+   }
+
+   return (
+      <div className="flex flex-col gap-5">
+         {reviewData?.reviews.map((review) => (
+            <div key={review.id}>
+               <div className="font-semibold">{review.author}</div>
+               <div>
+                  <StarRating value={review.rating} />
+               </div>
+               <p className="py-2">{review.content}</p>
+               <small>{new Date(review.createdAt).toLocaleDateString()}</small>
+            </div>
+         ))}
+         {reviewData?.reviews.length === 0 && <p>No reviews available.</p>}
+      </div>
+   );
+};
+
+export default ReviewList;
